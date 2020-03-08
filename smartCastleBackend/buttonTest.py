8b5b4145553f1fed743e1871dccccbbed52abcde
Kollock <maxklein96@web.de>
2020-03-08 19:11:36 +0100

    button test

--- smartCastleBackend/buttonTest.py
+++ smartCastleBackend/buttonTest.py
@@ -6,16 +6,22 @@ GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)#Button to GPIO23
 GPIO.setup(24, GPIO.OUT)  #LED to GPIO24
-print('Button Pressed...' + datetime.now())
+
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+
+print('Button Pressed...' + current_time)
 
 try:
     while True:
-         button_state = GPIO.input(23)
-         if button_state == False:
-             GPIO.output(24, True)
-             print('Button Pressed...' + datetime.now())
-             time.sleep(0.2)
-         else:
-             GPIO.output(24, False)
+        button_state = GPIO.input(23)
+        if button_state == False:
+            GPIO.output(24, True)
+            now = datetime.now()
+            current_time = now.strftime("%H:%M:%S")
+            print('Button Pressed...' + datetime.now())
+            time.sleep(0.2)
+        else:
+            GPIO.output(24, False)
 except:
     GPIO.cleanup()
