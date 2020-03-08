cdcb8ce6e91b873be677e1bd1eba02bf20e61082
Kollock <maxklein96@web.de>
2020-03-08 19:09:23 +0100

    button test

--- smartCastleBackend/buttonTest.py
+++ smartCastleBackend/buttonTest.py
@@ -1,17 +1,19 @@
 import RPi.GPIO as GPIO
 import time
+from datetime import datetime
 
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)#Button to GPIO23
 GPIO.setup(24, GPIO.OUT)  #LED to GPIO24
+print('Button Pressed...' + datetime.now())
 
 try:
     while True:
          button_state = GPIO.input(23)
          if button_state == False:
              GPIO.output(24, True)
-             print('Button Pressed...')
+             print('Button Pressed...' + datetime.now())
              time.sleep(0.2)
          else:
              GPIO.output(24, False)
